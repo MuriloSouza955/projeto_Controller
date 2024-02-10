5f5daaea2c18a9d5d457386ee5f3a1eeff3bfb2a
MuriloSouza955 <souza.murilo@gmail.com>
2024-02-09 22:26:56 -0300

    formulário inicial, faltando implementar o botão Enviar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
-import App from "./app/App";
+import Cadastro from "./components/CadastroInicial";
+
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {App}
-    </main>
+      <main>
+        <Cadastro />
+      </main>
   );
 }
